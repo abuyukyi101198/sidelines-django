8983b97f417f4498d7d932d669a77ea2f8a17ce3
abuyukyi101198 <acb3@outlook.com>
2024-07-10 22:34:52 +0300

    added token creation on POST

--- sidelines_django_app/views.py
+++ sidelines_django_app/views.py
@@ -1,16 +1,15 @@
-from .serializers import UserSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAdminUser
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserSerializer
 
 
 # Create your views here.
 class UserRecordView(APIView):
-
-    permission_classes = [IsAdminUser]
+    permission_classes = []
 
     def get(self, format=None):
         users = User.objects.all()
@@ -19,16 +18,14 @@ class UserRecordView(APIView):
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=ValueError):
-            serializer.create(validated_data=request.data)
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            {
-                "error": True,
-                "error_msg": serializer.error_messages,
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        if serializer.is_valid():
+            user = serializer.save()
+
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({
+                'token': token.key,
+                'user_id': user.id,
+                'username': user.username
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
