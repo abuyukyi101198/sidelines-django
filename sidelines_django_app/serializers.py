95148ed77da4dfc0976d2fe709b94c370ec31646
abuyukyi101198 <acb3@outlook.com>
2024-07-10 22:34:22 +0300

    added custom validation

--- sidelines_django_app/serializers.py
+++ sidelines_django_app/serializers.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError('Password must be at least 8 characters long.')
+        if not any(char.isdigit() for char in value):
+            raise serializers.ValidationError('Password must contain at least one digit.')
+        if not any(char.isalpha() for char in value):
+            raise serializers.ValidationError('Password must contain at least one letter.')
+        return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
 
     class Meta:
