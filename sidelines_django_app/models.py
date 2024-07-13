9dc77e4702127f26698748c76a32d9eb51403cb9
abuyukyi101198 <acb3@outlook.com>
2024-07-13 16:25:44 +0300

    added profile to UserSerializer

--- sidelines_django_app/models.py
+++ sidelines_django_app/models.py
@@ -1,8 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db import models
 
 
 # Create your models here.
@@ -14,16 +12,8 @@ class Profile(models.Model):
         STRIKER = "STRIKER", "Striker"
         ANY = "ANY", "Any"
 
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     overall_rating = models.FloatField(default=0.0)
     position = models.CharField(max_length=10, choices=Positions.choices, default=Positions.ANY)
     kit_number = models.IntegerField(default=10, validators=[MinValueValidator(1), MaxValueValidator(99)])
     join_date = models.DateField(auto_now_add=True)
-
-
-# @receiver(post_save, sender=User)
-# def create_or_update_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     else:
-#         instance.profile.save()
--- sidelines_django_app/serializers.py
+++ sidelines_django_app/serializers.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('overall_rating', 'position', 'kit_number', 'join_date',)
+        read_only_fields = ('overall_rating', 'join_date',)
 
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    profile = ProfileSerializer()
 
-    def validate_password(self, value):
+    @staticmethod
+    def validate_password(value):
         if len(value) < 8:
             raise serializers.ValidationError('Password must be at least 8 characters long.')
         if not any(char.isdigit() for char in value):
@@ -17,6 +27,7 @@ class UserSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
         user = User.objects.create_user(
             username=validated_data['username'],
             first_name=validated_data['first_name'],
@@ -24,16 +35,19 @@ class UserSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             password=validated_data['password']
         )
+        Profile.objects.create(user=user, **profile_data)
         return user
 
     class Meta:
         model = User
         fields = (
+            'id',
             'username',
             'first_name',
             'last_name',
             'email',
             'password',
+            'profile',
         )
         validators = [
             UniqueTogetherValidator(
