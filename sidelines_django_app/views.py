d1d61db0ff74bd1968b79d8d7660c5aa49803e1c
abuyukyi101198 <acb3@outlook.com>
2024-07-13 23:14:00 +0300

    extracted methods

--- sidelines_django_app/views.py
+++ sidelines_django_app/views.py
@@ -17,13 +17,21 @@ class UserRecordView(APIView):
     @staticmethod
     def get(request, user_id=None):
         if user_id is not None:
-            try:
-                user = User.objects.get(pk=user_id)
-                serializer = UserSerializer(user)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except User.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+            return UserRecordView.get_single_user(user_id)
 
+        return UserRecordView.get_all_users()
+
+    @staticmethod
+    def get_single_user(user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @staticmethod
+    def get_all_users():
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -33,9 +41,7 @@ class UserRecordView(APIView):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-
             token, created = Token.objects.get_or_create(user=user)
-
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -53,7 +59,8 @@ class FriendRequestView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, request_type=None):
+    @staticmethod
+    def get(request, request_type=None):
         profile = request.user.profile
 
         if request_type == 'sent':
@@ -66,7 +73,8 @@ class FriendRequestView(APIView):
         serializer = FriendRequestSerializer(friend_requests, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         from_profile = request.user.profile
 
         to_profile_id = request.data.get('to_profile')
@@ -75,50 +83,71 @@ class FriendRequestView(APIView):
         except Profile.DoesNotExist:
             return Response({'detail': 'Recipient profile not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-        if from_profile == to_profile:
-            return Response({'detail': 'Cannot send a friend request to yourself.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if FriendRequest.objects.filter(from_profile=from_profile, to_profile=to_profile).exists():
-            return Response({'detail': 'Friend request already sent.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if FriendRequest.objects.filter(from_profile=to_profile, to_profile=from_profile).exists():
-            return Response({'detail': 'Friend request already received from this user.'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        if to_profile in from_profile.friends.all():
-            return Response({'detail': 'This user is already your friend.'}, status=status.HTTP_400_BAD_REQUEST)
+        error = FriendRequestView.validate_friend_request(from_profile, to_profile)
+        if error:
+            return Response({'detail': error}, status=status.HTTP_400_BAD_REQUEST)
 
         friend_request = FriendRequest(from_profile=from_profile, to_profile=to_profile)
         friend_request.save()
         serializer = FriendRequestSerializer(friend_request)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @staticmethod
+    def validate_friend_request(from_profile, to_profile):
+        if from_profile == to_profile:
+            return 'Cannot send a friend request to yourself.'
+        if FriendRequest.objects.filter(from_profile=from_profile, to_profile=to_profile).exists():
+            return 'Friend request already sent.'
+        if FriendRequest.objects.filter(from_profile=to_profile, to_profile=from_profile).exists():
+            return 'Friend request already received from this user.'
+        if to_profile in from_profile.friends.all():
+            return 'This user is already your friend.'
+        return None
+
 
 class FriendRequestActionView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, friend_request_id, action):
+    @staticmethod
+    def post(request, friend_request_id, action):
         try:
             friend_request = FriendRequest.objects.get(pk=friend_request_id)
-            if action == 'accept':
-                if friend_request.to_profile.user != request.user:
-                    return Response({'detail': 'You can only accept friend requests sent to you.'},
-                                    status=status.HTTP_403_FORBIDDEN)
-                friend_request.accept()
-            elif action == 'ignore':
-                if friend_request.to_profile.user != request.user:
-                    return Response({'detail': 'You can only ignore friend requests sent to you.'},
-                                    status=status.HTTP_403_FORBIDDEN)
-                friend_request.ignore()
-            elif action == 'withdraw':
-                if friend_request.from_profile.user != request.user:
-                    return Response({'detail': 'You can only withdraw friend requests sent by you.'},
-                                    status=status.HTTP_403_FORBIDDEN)
-                friend_request.delete()
-            else:
-                return Response({'detail': 'Invalid action.'}, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response(status=status.HTTP_200_OK)
+            return FriendRequestActionView.handle_action(request.user, friend_request, action)
         except FriendRequest.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @staticmethod
+    def handle_action(user, friend_request, action):
+        if action == 'accept':
+            return FriendRequestActionView.accept_request(user, friend_request)
+        elif action == 'ignore':
+            return FriendRequestActionView.ignore_request(user, friend_request)
+        elif action == 'withdraw':
+            return FriendRequestActionView.withdraw_request(user, friend_request)
+        else:
+            return Response({'detail': 'Invalid action.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def accept_request(user, friend_request):
+        if friend_request.to_profile.user != user:
+            return Response({'detail': 'You can only accept friend requests sent to you.'},
+                            status=status.HTTP_403_FORBIDDEN)
+        friend_request.accept()
+        return Response(status=status.HTTP_200_OK)
+
+    @staticmethod
+    def ignore_request(user, friend_request):
+        if friend_request.to_profile.user != user:
+            return Response({'detail': 'You can only ignore friend requests sent to you.'},
+                            status=status.HTTP_403_FORBIDDEN)
+        friend_request.ignore()
+        return Response(status=status.HTTP_200_OK)
+
+    @staticmethod
+    def withdraw_request(user, friend_request):
+        if friend_request.from_profile.user != user:
+            return Response({'detail': 'You can only withdraw friend requests sent by you.'},
+                            status=status.HTTP_403_FORBIDDEN)
+        friend_request.delete()
+        return Response(status=status.HTTP_200_OK)
