14fdae2c4b518464aa9a1c4d13e47771de2bcad0
abuyukyi101198 <acb3@outlook.com>
2024-07-14 20:07:49 +0300

    converted models.py to package

--- sidelines_django_app/models/FriendRequest.py
+++ sidelines_django_app/models/FriendRequest.py
@@ -1,23 +1,6 @@
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-
-# Create your models here.
-class Profile(models.Model):
-    class Positions(models.TextChoices):
-        GOALKEEPER = "GOALKEEPER", "Goalkeeper"
-        DEFENDER = "DEFENDER", "Defender"
-        MIDFIELDER = "MIDFIELDER", "Midfielder"
-        STRIKER = "STRIKER", "Striker"
-        ANY = "ANY", "Any"
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    overall_rating = models.FloatField(default=0.0)
-    position = models.CharField(max_length=10, choices=Positions.choices, default=Positions.ANY)
-    kit_number = models.IntegerField(default=10, validators=[MinValueValidator(1), MaxValueValidator(99)])
-    friends = models.ManyToManyField('self', symmetrical=True)
-    join_date = models.DateField(auto_now_add=True)
+from sidelines_django_app.models import Profile
 
 
 class FriendRequest(models.Model):
---
+++ sidelines_django_app/models/__init__.py
@@ -0,0 +1,2 @@
+from .Profile import Profile
+from .FriendRequest import FriendRequest
