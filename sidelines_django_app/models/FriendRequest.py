4b0096ee91e64165a880d57b5ac334ef24a3f67c
abuyukyi101198 <acb3@outlook.com>
2024-07-14 22:56:45 +0300

    removed unnecessary save() override

--- sidelines_django_app/models/FriendRequest.py
+++ sidelines_django_app/models/FriendRequest.py
@@ -15,18 +15,3 @@ class FriendRequest(models.Model):
 
     def ignore(self):
         self.delete()
-
-    def save(self, *args, **kwargs):
-        if self.from_profile == self.to_profile:
-            raise ValueError("Cannot send a friend request to yourself.")
-
-        if FriendRequest.objects.filter(from_profile=self.from_profile, to_profile=self.to_profile).exists():
-            raise ValueError("Friend request already sent.")
-
-        if FriendRequest.objects.filter(from_profile=self.to_profile, to_profile=self.from_profile).exists():
-            raise ValueError("Friend request already received from this user.")
-
-        if self.to_profile in self.from_profile.friends.all():
-            raise ValueError("This user is already your friend.")
-
-        super().save(*args, **kwargs)
