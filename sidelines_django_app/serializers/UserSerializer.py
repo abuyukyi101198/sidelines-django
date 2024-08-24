74a1c6b3a96252e22ce3ba72605a97d7efe7a30d
abuyukyi101198 <acb3@outlook.com>
2024-08-24 17:16:09 +0300

    credential-only user creation

--- sidelines_django_app/serializers/UserSerializer.py
+++ sidelines_django_app/serializers/UserSerializer.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
 from sidelines_django_app.models import Profile
@@ -10,23 +10,20 @@ from sidelines_django_app.serializers.ProfileSerializer import ProfileSerializer
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    profile = ProfileSerializer()
+    profile = ProfileSerializer(required=False)
 
     def validate_password(self, value):
-        validate_password(value, self.instance)
+        validate_password(value)
         return value
 
     def create(self, validated_data):
-        profile_data = validated_data.pop('profile')
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            email=validated_data['email']
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['email'].split('@')[0]
         )
         user.set_password(validated_data['password'])
         user.save()
-        Profile.objects.create(user=user, **profile_data)
+        Profile.objects.create(user=user)
         return user
 
     def update(self, instance, validated_data):
@@ -38,17 +35,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'id',
-            'username',
-            'first_name',
-            'last_name',
             'email',
             'password',
             'profile',
         )
         extra_kwargs = {'password': {'write_only': True}}
-        validators = [
-            UniqueTogetherValidator(
-                queryset=User.objects.all(),
-                fields=['username', 'email']
-            )
-        ]
