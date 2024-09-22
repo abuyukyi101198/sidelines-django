7cdc061d7ce348b020e926c2d3aa268a449b2f35
abuyukyi101198 <acb3@outlook.com>
2024-09-22 13:45:38 +0300

    added media support

--- sidelines_django/urls.py
+++ sidelines_django/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sidelines_django_app.urls', namespace='api')),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
