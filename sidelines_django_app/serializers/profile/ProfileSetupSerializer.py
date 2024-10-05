ba04e372dcde61df642b2cb96692fb60b9cd82f0
abuyukyi101198 <acb3@outlook.com>
2024-10-06 00:47:33 +0300

    fixed setup complete too many args

--- sidelines_django_app/serializers/profile/ProfileSetupSerializer.py
+++ sidelines_django_app/serializers/profile/ProfileSetupSerializer.py
@@ -4,14 +4,8 @@ from sidelines_django_app.models import Profile
 
 
 class ProfileSetupSerializer(serializers.ModelSerializer):
-    setup_complete = serializers.SerializerMethodField()
-
     class Meta:
         model = Profile
         fields = (
             'profile_picture', 'date_of_birth', 'positions', 'kit_number', 'setup_complete',
         )
-        read_only_fields = ('setup_complete',)
-
-    def get_setup_complete(self):
-        return True
